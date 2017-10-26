efc929b2b5a5e5a5046bc6537eaf6484e3479368
Artem <it.chubaka@gmail.com>
2017-10-26 15:38:58 +0300

    Changed HelloWorldimpl

--- src/main/java/com/teaminternational/web/HttpServlet.java
+++ src/main/java/com/teaminternational/web/HttpServlet.java
@@ -1,8 +1,28 @@
 package com.teaminternational.web;
 
+import com.teaminternational.HelloWorldGenerator;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet("/")
 public class HttpServlet  extends BaseHttpServlet{
-    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HelloWorldGenerator helloWorldGenerator = getBean("Hello world");
+        helloWorldGenerator.sayHello();
+        resp.getWriter().write("<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <title>Hello world</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "\n" +
+                "</body>\n" +
+                "</html>");
+    }
 }
